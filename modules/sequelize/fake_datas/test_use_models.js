6a10541f127947389700bf143d3f05756c10979a
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2020-02-06 11:31:29 +0100

    add "migrate" commande to package.json and optimize test_use_models.js file

--- modules/sequelize/fake_datas/test_use_models.js
+++ modules/sequelize/fake_datas/test_use_models.js
@@ -1,19 +1,11 @@
 const {sequelize} = require("../../sequelize");
 const db = require("../../../models");
 
-sequelize.authenticate().then(() => {
-    (async user_id => {
-        let user = await db.User.findOne({
-            where: {
-                id: user_id
-            }
-        });
+sequelize.authenticate()
+    .then(() => (async user_id => {
+        let user = await db.User.findOne({where: {id: user_id}});
         let userDiscussions = [];
-        for(let discussion of await user.MyDiscussions) {
-            userDiscussions.push(await discussion.JSON)
-        }
+        for(let discussion of await user.MyDiscussions) userDiscussions.push(await discussion.JSON);
         return userDiscussions;
-    })(1).then(discussions => {
-        console.log(discussions[0].messages);
-    });
-});
+    })(1))
+    .then(discussions => console.log('Messages de la discussion `' + discussions[0].name + '`: ', discussions[0].messages));
